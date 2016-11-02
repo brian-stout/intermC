e451ccc9db2739e828736c88614fae1d6e5e6e10
Brian Stout <brianjonstout@gmail.com>
2016-11-02 11:27:25 -0400

    Add printed list of movies.

--- Day3/malloc.c
+++ Day3/malloc.c
@@ -2,23 +2,47 @@
 #include <string.h>
 #include <stdlib.h>
 #include <sysexits.h>
+#include <stdbool.h>
 
 char *get_movie(void);
 
 int main(void)
 {
-	char **movie_list = malloc(1 * sizeof(*movie_list));
+	size_t list_len = 1;
+	char **movie_list = malloc(list_len * sizeof(*movie_list));
 
 	if(!movie_list){
 		return EX_OSERR;
 	}
 	
-
+	do{
 		printf("Enter a movie title: ");
 		char *movie = get_movie();
-		movie_list[0] = movie;
-	
-	free(movie);
+		movie_list[list_len - 1] = movie;
+		
+		++list_len;
+
+		//WRONG BAD BADONG
+		//movie_list = realloc(movie_list, list_len * sizeof(*movie_list));
+
+		void *tmp = realloc(movie_list, list_len * sizeof(*movie_list));
+		if(tmp){
+			movie_list = tmp;
+		} 
+		else{
+			//What to do when shit fails.
+			break;
+		}
+		//Gives programmer a thing to print to
+		movie_list[list_len - 1] = NULL;
+	} while (movie_list[list_len - 2] && strlen(movie_list[list_len-2]) > 1);
+
+	char **curr_movie = movie_list;
+	while(*curr_movie){
+		printf("%s", *curr_movie);
+		++curr_movie;
+	}
+
 	free(movie_list);
 }
 
