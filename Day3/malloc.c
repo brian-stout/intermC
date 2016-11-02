32618429556cf71fb0020b6f1e3c1d457d4346e5
Brian Stout <brianjonstout@gmail.com>
2016-11-02 10:24:45 -0400

    Checks on malloc.

--- Day3/malloc.c
+++ Day3/malloc.c
@@ -1,11 +1,40 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char *get_movie(void);
 
 int main(void)
 {
 	printf("Enter a movie title: ");
+	char *first = get_movie();
 
+	printf("Enter a movie title: ");
+	char *second = get_movie();
+
+	printf("%s", first);
+	printf("%s", second);
+
+	free(first);
+	free(second);
+}
+
+
+//Returns a newly malloc()'d section of memory that much be free()'d 
+// By the caller
+char *get_movie(void)
+{
 	char buf[64];
 	fgets(buf, sizeof(buf), stdin);
 
-	printf("%s", buf);
+	size_t sz = strlen(buf) + 1;
+
+	char *movie = malloc(sz);
+	if(!movie){
+		return NULL;
+	}
+
+	strncpy(movie, buf, sz);
+
+	return movie;
 }
