00918723a1cf76bd8a49f35b8ff0e8e80843ee81
Brian Stout <brianjonstout@gmail.com>
2016-11-02 08:41:06 -0400

    Prep the c source for being liamified.

--- Day2/c5e1.c
+++ Day2/c5e1.c
@@ -1,54 +1,32 @@
 #include <stdio.h>
 #include <string.h>
 
-/*
-struct word{
-	int word_count = 1;
-	char word[16];
-};
-*/
+const size_t NUMBER_OF_WORDS = 256;
 
-
-
-void print_word(int count, char cur[]);
-
-int main(void)
+int main(int argc, char *argv[])
 {
-	
-	FILE *fp = fopen("lol", "r");
+	if(argc != 2){
+		perror("USAGE:  Program <file name>");
+		return 1;
+	}
+	FILE *fp = fopen(argv[1], "r");
 	//fopen can return NULL on error
 	//NULL we can treat like a false value
 	if(!fp){
 		perror("Unable to open file for reading");
 		return 1;
 	}
+
+	char *words[NUMBER_OF_WORDS];
+	int counts[NUMBER_OF_WORDS];
+	
 	int c;
-	char cur[10];
-	int count = 0;
-	while(c = fgetc(fp) && c != EOF){
-
-		if((c == ' ') || c == '\n') || c == EOF)){	
-			print_word(count, cur[]);	
-		}
-		cur[count++] = c;
+	while(c != EOF){
+			c = fgetc(fp);
+			printf("%c\n", c);	
 	}
 
-}
+	fclose(fp);
 
-void print_word(int count, char cur[])
-{
-	for(int i = count; i > 0; --i){
-		printf("%c", cur[i]);
-	}
-	printf("\n");
 }
-
-/*
-void detect_word(char line[])
-{
-	for(size_t i = 0; i < strlen(line); ++i){
-		printf("%c \n", line[i]);
-	}
-}
-*/
 
