580968f227ff15d3103c2a673b6d66c2546b7b77
Brian Stout <brianjonstout@gmail.com>
2016-11-02 08:46:14 -0400

    Enum C magic

--- Day2/c5e1.c
+++ Day2/c5e1.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sysexits.h>
 #include <string.h>
 
-const size_t NUMBER_OF_WORDS = 256;
+enum {NUM_OF_WORDS = 256};
 
 int main(int argc, char *argv[])
 {
 	if(argc != 2){
 		perror("USAGE:  Program <file name>");
-		return 1;
+		return EX_NOINPUT;
 	}
 	FILE *fp = fopen(argv[1], "r");
 	//fopen can return NULL on error
 	//NULL we can treat like a false value
 	if(!fp){
 		perror("Unable to open file for reading");
-		return 1;
+		return EX_NOINPUT;
 	}
 
-	char *words[NUMBER_OF_WORDS];
-	int counts[NUMBER_OF_WORDS];
+	char *words[NUM_OF_WORDS];
+	int counts[NUM_OF_WORDS];
 	
 	int c;
 	while(c != EOF){
